9e0bfc01c715b5987deaf3055e2da608f08481ef
Alberto Gaona <albertoo_3c@hotmail.com>
2023-04-06 11:59:21 -0600

    add pre-commit plus minor changes

--- gptpose/cli.py
+++ gptpose/cli.py
@@ -1,41 +1,58 @@
 """
 gptpose CLI
 """
-    
-import click 
+
+import click
 import os
 import getpass
 
 from gptpose.generator import PoseGenerator
 
+
 def run(
     pose_description: str,
-    output_path: str
-):
-    """generates a pose image based on a body pose description.
-    """
+    output_path: str,
+    model_name: str,
+) -> None:
+    """generates a pose image based on a body pose description."""
 
     generator = PoseGenerator(
-        pose_description=pose_description, 
-        output_path=output_path
+        pose_description=pose_description, output_path=output_path
     )
 
-    generator.generate_pose()
+    _ = generator.generate_pose(model_name=model_name)
 
 
 @click.command()
-@click.option("-d", "--description", "pose_description", required=True, help="The body pose description")
-@click.option("-o", "--output-path", "output_path", required=True, help="The path to save the pose images.")
-def main(
-    pose_description: str,
-    output_path: str
-):
+@click.option(
+    "-d",
+    "--description",
+    "pose_description",
+    required=True,
+    help="The body pose description",
+)
+@click.option(
+    "-o",
+    "--output-path",
+    "output_path",
+    required=True,
+    help="The path to save the pose images.",
+)
+@click.option(
+    "-n",
+    "--model-name",
+    "model_name",
+    required=False,
+    default="GPT-4",
+    help="The OpenAI model name you want to use. https://platform.openai.com/docs/models",
+)
+def main(pose_description: str, output_path: str, model_name: str = "gpt-4"):
     if not os.environ.get("OPENAI_API_KEY"):
         os.environ["OPENAI_API_KEY"] = getpass.getpass(
             prompt="Enter your OpenAI API key: "
         )
 
-    run(pose_description, output_path) 
+    run(pose_description, output_path, model_name)
 
 
 if __name__ == "__main__":
--- gptpose/generator.py
+++ gptpose/generator.py
@@ -1,46 +1,59 @@
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate 
+from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 from gptpose.templates import POSE_VALUES_TEMPLATE
 from gptpose.utils import build_image, get_logger
 
+
 class PoseGenerator:
-    """generates a pose image for the description.
-    """
-    def __init__(
-        self, 
-        pose_description:str,
-        output_path: str
-    ) -> None:
+    """generates a pose image for the description."""
+
+    def __init__(self, pose_description: str, output_path: str) -> None:
         """constructor
 
         Args:
-            pose_description (str): body pose description in plain english.
+            pose_description (str): body pose description.
             output_path (str): path to save pose image.
         """
-        self.pose_description = pose_description
-        self.output_path = output_path
+        # initialize logger
         self.logger = get_logger("gptpose")
 
-    def generate_pose(self):
-        self.logger.info("asking GPT to set the pose based on the pose body description...")
-        chain = self._create_chain()
+        self.pose_description = pose_description
+        self.output_path = output_path
+
+    def generate_pose(self, model_name):
+        """builds a pose image based on GPT coords suggestions.
+
+        Returns:
+            pose_image: pose image.
+        """
+        self.logger.info(
+            "Asking GPT to define the pose coords based on the pose body description..."
+        )
+        chain = self._create_chain(model_name)
         gpt_response = chain.run(self.pose_description)
         self.logger.info(f"GPT answer: {gpt_response}")
 
-        self.logger.info(f"building pose...")
+        self.logger.info("building pose image...")
         pose_image = build_image(gpt_response)
 
-        self.logger.info(f"saving pose in {self.output_path}")
+        self.logger.info(f"saving pose image in {self.output_path}")
         pose_image.save(self.output_path)
 
+        return pose_image
 
-    def _create_chain(self):
-        # this task is not well handled by GPT-3, it requires GPT-4
-        llm = OpenAI()
+    def _create_chain(self, model_name: str):
+        """create chain object to send request to GPT.
+
+        Args:
+            model_name (str): OpenAI model name to use.
+
+        Returns:
+            LLMChain: chain object.
+        """
+        llm = OpenAI(model_name=model_name)
         prompt = PromptTemplate(
-            input_variables=["pose_description"],
-            template = POSE_VALUES_TEMPLATE
+            input_variables=["pose_description"], template=POSE_VALUES_TEMPLATE
         )
         return LLMChain(prompt=prompt, llm=llm)
